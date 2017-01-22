3e4104284965e141c9bc1d4db14fde5f52a3a96a
Andrew Dushane <andrewdushane@gmail.com>
2017-01-22 14:21:00 -0500

    Auth/login screen

--- src/components/containers/Auth.jsx
+++ src/components/containers/Auth.jsx
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Redirect from 'react-router/Redirect';
 import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+import Navigation from '../navigation/Navigation';
 import { loggedInSelector } from '../../selectors/session';
 import { loadLocalToken } from '../../actions/session';
+import githubLogo from '../../images/github-logo-64.png'
+
+const authExplanation = "You'll need to connect your GitHub account so we can access your repositories and generate your reports."
+const authAssurance = "We'll never use your GitHub account information for anything other than saving your GitHub Reporter settings and exporting GitHub data at your request."
 
 const apiRoot = process.env.REACT_APP_API_URL || '';
 
@@ -19,7 +25,27 @@ class Auth extends Component {
     }
     return (
       <div>
-        <RaisedButton primary label="Authorize" href={`${apiRoot}/api/authorize-github`} />
+        <Navigation />
+        <div className="auth">
+          <Card className="explanation">
+            <CardHeader
+              title="Connect Your GitHub Account"
+              subtitle="Authorize Github Reporter so we can start creating your reports"
+              avatar={githubLogo}
+              style={{ backgroundColor: '#FAFAFA' }}
+            />
+            <CardText>
+              <p>{authExplanation}</p>
+              <p>{authAssurance}</p>
+            </CardText>
+          </Card>
+          <RaisedButton
+            primary
+            label="Connect to GitHub"
+            href={`${apiRoot}/api/authorize-github`}
+            fullWidth
+          />
+        </div>
       </div>
     );
   }
---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const Navigation = () => (
+  <AppBar
+    title="GitHub Reporter"
+    showMenuIconButton={false}
+  />
+);
+
+export default Navigation;
